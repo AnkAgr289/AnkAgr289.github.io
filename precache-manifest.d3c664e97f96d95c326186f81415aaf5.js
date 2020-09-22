ba7e2ad8ee355a4b058c2db6d1b17456a267b225
AnkAgr289 <ankur@brickredsystems.ca>
2020-09-22 01:29:10 -0700

    Updates

---
+++ precache-manifest.d3c664e97f96d95c326186f81415aaf5.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b88b9645f124f9e29f9e19e12c0be19d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3ff279adecf94a93b60",
+    "url": "/static/css/main.c52d75e3.chunk.css"
+  },
+  {
+    "revision": "c3b79e5c8b5a24b09b52",
+    "url": "/static/js/2.bcd2cf02.chunk.js"
+  },
+  {
+    "revision": "cb5c9bef82b1b500bbeb6e747a258fde",
+    "url": "/static/js/2.bcd2cf02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3ff279adecf94a93b60",
+    "url": "/static/js/main.63cc16e6.chunk.js"
+  },
+  {
+    "revision": "e13740ac281a0e7f8937",
+    "url": "/static/js/runtime-main.1c8f4745.js"
+  },
+  {
+    "revision": "03c2e8629c48ab3a4791f8354a902dd0",
+    "url": "/static/media/TeamsAssets-Light.03c2e862.eot"
+  },
+  {
+    "revision": "4521ef7e401ab922adbe6019d2c1b015",
+    "url": "/static/media/TeamsAssets-Light.4521ef7e.woff"
+  },
+  {
+    "revision": "5c50f55fa067736aabd6a0248fa193f2",
+    "url": "/static/media/TeamsAssets-Light.5c50f55f.ttf"
+  },
+  {
+    "revision": "9c0b4d34e890dee987096be52b499ce5",
+    "url": "/static/media/TeamsAssets-Light.9c0b4d34.svg"
+  },
+  {
+    "revision": "d7d7eb79c2b2c8d705bd24e50090817b",
+    "url": "/static/media/TeamsAssets-Light.d7d7eb79.woff2"
+  },
+  {
+    "revision": "2de16aabf919bce6480fe5be06fd5928",
+    "url": "/static/media/TeamsAssets-Regular.2de16aab.woff2"
+  },
+  {
+    "revision": "2ed5eaeaa85b5ae4fcb6fa9451934437",
+    "url": "/static/media/TeamsAssets-Regular.2ed5eaea.woff"
+  },
+  {
+    "revision": "76f7e5927a28f5177c391e872afca92d",
+    "url": "/static/media/TeamsAssets-Regular.76f7e592.ttf"
+  },
+  {
+    "revision": "c5a2112b4020146201ccb443a2adfacc",
+    "url": "/static/media/TeamsAssets-Regular.c5a2112b.eot"
+  },
+  {
+    "revision": "de8a9eb155049912556a1e3d9000709a",
+    "url": "/static/media/TeamsAssets-Regular.de8a9eb1.svg"
+  }
+]);
